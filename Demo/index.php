de00a457f41a1513dd307cd1627c15b081b62a67
aranhu <aran.hu@oet-china.com>
2015-05-14 17:55:47 +0800

    first

---
+++ Demo/index.php
@@ -0,0 +1,10 @@
+<?php
+include_once("../Demo/SendTemplateSMS.php");
+/**
+ * Created by PhpStorm.
+ * User: aranhu
+ * Date: 15/5/14
+ * Time: 上午10:56
+ */
+sendTemplateSMS("18600602987","Hello World",1);
+echo '123';
